6697626eea594fc5710d9961c228cc1b526be8d2
arianarmw <arianarahmawati0910@gmail.com>
2025-01-02 21:22:49 +0700

    FEAT: Add new method like update, soft delete, and get by id

--- src/main/java/app/sales/service/ProductService.java
+++ src/main/java/app/sales/service/ProductService.java
@@ -2,10 +2,17 @@ package app.sales.service;
 
 import java.util.List;
 
+import app.sales.dto.product.ShowProductResponse;
 import app.sales.entity.Product;
 
 public interface ProductService {
     Product addProduct(Product product);
 
-    List<Product> getAllProducts();
+    List<ShowProductResponse> getAllProducts();
+
+    Product getProductById(Integer productId);
+
+    Product updateProduct(Integer productId, Product product);
+
+    Product softDeleteProduct(Integer productId);
 }
