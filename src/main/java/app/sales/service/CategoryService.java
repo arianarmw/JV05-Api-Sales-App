5a9e379f5c4ebbd1cbc93fbeec425db30b9ffc7f
arianarmw <arianarahmawati0910@gmail.com>
2025-01-03 15:00:28 +0700

    UPDATE: Memperbaiki method untuk categoires

--- src/main/java/app/sales/service/CategoryService.java
+++ src/main/java/app/sales/service/CategoryService.java
@@ -2,8 +2,9 @@ package app.sales.service;
 
 import java.util.List;
 
+import app.sales.dto.ApiResponse;
 import app.sales.dto.category.CategoryResponse;
 
 public interface CategoryService {
-    List<CategoryResponse> getAllCategories();
+    ApiResponse<List<CategoryResponse>> getAllCategories();
 }
