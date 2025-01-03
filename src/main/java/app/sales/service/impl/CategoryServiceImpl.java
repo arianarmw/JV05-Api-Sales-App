9160122c700eb7b659bc28c4af648929b685d2a1
arianarmw <arianarahmawati0910@gmail.com>
2025-01-03 15:01:07 +0700

    UPDATE: Memperbaiki method untuk categoires

--- src/main/java/app/sales/service/impl/CategoryServiceImpl.java
+++ src/main/java/app/sales/service/impl/CategoryServiceImpl.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import app.sales.dto.ApiResponse;
 import app.sales.dto.category.CategoryResponse;
 import app.sales.entity.Category;
 import app.sales.repository.CategoryRepository;
@@ -18,11 +19,19 @@ public class CategoryServiceImpl implements CategoryService {
     private CategoryRepository categoryRepository;
 
     @Override
-    public List<CategoryResponse> getAllCategories() {
+    public ApiResponse<List<CategoryResponse>> getAllCategories() {
         List<Category> categories = categoryRepository.findAll();
-        return categories.stream()
+
+        List<CategoryResponse> categoryResponses = categories.stream()
                 .map(category -> new CategoryResponse(category.getCategoryId(), category.getCategoryName()))
                 .collect(Collectors.toList());
-    }
 
+        ApiResponse<List<CategoryResponse>> response = new ApiResponse<>();
+        response.setData(categoryResponses);
+        response.setMessage(categoryResponses.isEmpty() ? "Tidak ada kategori yang ditemukan."
+                : "Berhasil mengambil data kategori.");
+        response.setStatusCode(200);
+        response.setStatus("Sukses");
+        return response;
+    }
 }
