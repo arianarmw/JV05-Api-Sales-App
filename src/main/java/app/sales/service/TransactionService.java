4cce77f47b372f8535de734dabec894f79ce35e1
arianarmw <arianarahmawati0910@gmail.com>
2025-01-03 17:19:16 +0700

    UPDATE: Memperbaiki method

--- src/main/java/app/sales/service/TransactionService.java
+++ src/main/java/app/sales/service/TransactionService.java
@@ -3,14 +3,13 @@ package app.sales.service;
 import java.util.List;
 import java.util.UUID;
 
+import app.sales.dto.transaction.TransactionResponse;
 import app.sales.entity.Transaction;
 
 public interface TransactionService {
-    Transaction createTransaction(Integer productId, int quantity);
+    TransactionResponse createTransaction(Integer productId, int quantity);
 
     Transaction refundTransaction(UUID transactionId, int quantity);
 
-    Transaction getTransactionById(UUID transactionId);
-
-    List<Transaction> getAllTransactions();
+    List<TransactionResponse> getAllTransactions();
 }
