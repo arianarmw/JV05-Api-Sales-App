1985ae02eda727b1f761b7c104df53d1bf8aab86
arianarmw <arianarahmawati0910@gmail.com>
2025-01-03 17:17:14 +0700

    UPDATE: Memperbaiki respon

--- src/main/java/app/sales/dto/transaction/TransactionResponse.java
+++ src/main/java/app/sales/dto/transaction/TransactionResponse.java
@@ -1,16 +1,19 @@
 package app.sales.dto.transaction;
 
+import java.util.UUID;
+
 import app.sales.dto.product.ProductResponse;
 import app.sales.dto.user.UserResponse;
 import lombok.Data;
 
 @Data
 public class TransactionResponse {
+    private UUID transactionId;
     private UserResponse user;
     private ProductResponse product;
     private int quantity;
     private double subtotal;
     private double total;
     private String transactionDate;
-
+    private double refundAmount;
 }
