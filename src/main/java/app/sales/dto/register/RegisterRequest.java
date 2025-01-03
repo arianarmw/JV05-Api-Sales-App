c7a1cc595393a18df16036e6cf5ac28fd0593bb0
arianarmw <arianarahmawati0910@gmail.com>
2025-01-03 14:18:06 +0700

    UPDATE: Memperbaiki pengambilan request untuk register

--- src/main/java/app/sales/dto/register/RegisterRequest.java
+++ src/main/java/app/sales/dto/register/RegisterRequest.java
@@ -7,5 +7,5 @@ public class RegisterRequest {
     private String username;
     private String fullname;
     private String password;
-    private String retypePassword;
+    private String confirmPassword;
 }
